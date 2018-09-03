7a0f6d3bddd641cb268ac1dbea9ed19216ba7c4b
Pajani <pajani.arjunan@gmail.com>
2018-09-03 14:42:20 +0530

    environments committed

--- ThandoraWebApp/src/environments/environment.prod.ts
+++ ThandoraWebApp/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    
+    apiKey: "AIzaSyBZ6qq9OJbIKvYraCIOOGqNyp_19yBXm2o",
+    authDomain: "thandora-58cba.firebaseapp.com",
+    databaseURL: "https://thandora-58cba.firebaseio.com",
+    projectId: "thandora-58cba",
+    storageBucket: "thandora-58cba.appspot.com",
+    messagingSenderId: "391118300920"
+
+    
+  }
 };
